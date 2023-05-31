5367164378c176893436459a453c948d4adedd98
suganthanhaarmikan <135023505+suganthanhaarmikan@users.noreply.github.com>
2023-05-31 17:53:55 +0530

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,10 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python311/myfirstprogram.py
+Hello, World
+>>> 
+= RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python311/myfirstprogram.py
+Hello, World
+What is your name? Haarmikan
+Hello  Haarmikan
